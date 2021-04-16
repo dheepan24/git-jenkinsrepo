0757e1e73a95f8cc862bba153d596c7ecb31e992
dheepan.rajasekar@gmail.com <dheepan.rajasekar@gmail.com>
2021-04-16 06:38:37 +0000

    all

--- Iintegrategitwithjenkins.sh
+++ Iintegrategitwithjenkins.sh
@@ -1,8 +1,8 @@
 #/bin/bash
 cd /gitproject
-touch filecreatedthroughjenkins.txt
-echo "i am successful" >> filecreatedthroughjenkins.txt
-chmod 777 filecreatedthroughjenkins.txt
-cat filecreatedthroughjenkins.txt
-git add filecreatedthroughjenkins.txt
-git commit filecreatedthroughjenkins.txt -m "adding file1 through jenkins"
+touch filecreatedthroughjenkins_2.txt
+echo "i am successful" >> filecreatedthroughjenkins_2.txt
+cat filecreatedthroughjenkins_2.txt
+git add filecreatedthroughjenkins_2.txt
+git commit filecreatedthroughjenkins_2.txt -m "adding file1 through jenkins"
+git push origin master
